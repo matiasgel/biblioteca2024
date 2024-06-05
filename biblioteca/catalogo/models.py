e3f2391e22d9317c3ecd4b16851407ad6ef8b9f6
Ivan D'Uva <duva.ivan@outlook.com>
2024-06-05 18:51:45 -0300

    Agrega atributo publicado al libro

--- biblioteca/catalogo/models.py
+++ biblioteca/catalogo/models.py
@@ -17,6 +17,7 @@ class Libro(models.Model):
     resumen = models.TextField(max_length=1000, help_text="Ingrese un breve resumen del libro")
     isbn = models.CharField('ISBN', max_length=13, help_text='13 Caracteres <a href="https://www.isbn-international.org/content/what-isbn">ISBN</a>')
     genero = models.ForeignKey(Genero, on_delete=models.SET_NULL, null=True)
+    publicado = models.BooleanField(default=False)
 
     def __str__(self):
         return self.titulo
